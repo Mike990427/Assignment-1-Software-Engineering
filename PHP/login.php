b9d5858a5f7e3ae2113371f2a60d96c4457b5280
Mike <Xwu0580@conestogac.on.ca>
2022-06-13 12:11:39 -0400

    question 8 fixed

--- PHP/login.php
+++ PHP/login.php
@@ -8,11 +8,11 @@ $submitted = !empty($_POST);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login PHP</title>
 </head>
 <body>
     <p>Form submited? <?php echo (int) $submitted; ?></p>
-    <p>Login info</p>
+    <p>Login information</p>
     <ul>
         <li>Username: <?php echo $_POST['username'] ?></li>
         <li>Password: <?php echo $_POST['password'] ?></li>
